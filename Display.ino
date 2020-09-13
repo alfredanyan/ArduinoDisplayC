77152c940729c1350ae58b55fc6f587f2ca16c37
Alfred Anyan <46558691+alfredanyan@users.noreply.github.com>
2020-09-13 10:09:10 +0000

    Create Display.ino

---
+++ Display.ino
@@ -0,0 +1,15 @@
+#include "SevSeg.h"
+SevSeg sevseg;
+
+const int buttonPin = 13
+int buttonState = 0;
+int lastButtonState = 0;
+
+void setup()(
+Serial.begin(9600);
+pinMode(buttonPin, INPUT);
+byte numDigits = 1;
+byte digitPins
+
+)
+
